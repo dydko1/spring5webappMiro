d0b0e09b760810d7513608b7e70413ddadc8ac9f
dydko1 <miroslaw.dyduch@gmail.com>
2021-04-07 07:38:22 +0200

    ModelView added

--- src/main/java/guru/springframework/spring5webapp/HelloController.java
+++ src/main/java/guru/springframework/spring5webapp/HelloController.java
@@ -1,39 +1,33 @@
 package guru.springframework.spring5webapp;
-
 import org.springframework.stereotype.Controller;
-
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
-
 public class HelloController {
 
-    /*
     @GetMapping({"/", "/hello"})
-
-    public String hello(Model model, @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
-
-        model.addAttribute("name", name);
-
-        return "hello";
-
-    }
-
-     */
-
-    @GetMapping({
-            "/",
-            "/hello"
-    })
-    public String hello(@RequestParam(value = "name", defaultValue = "World",
-            required = true) String name, Model model) {
+    public String hello(@RequestParam(value = "name", defaultValue = "World", required = true) String name, Model model) {
         model.addAttribute("name", name);
         return "hello";
     }
 
-
+    @RequestMapping("courses")
+    public ModelAndView courses(@RequestParam("cname") String coursename)
+    {
+        //String cname= req.getParameter("cname");
+        System.out.println("The course name is: " + coursename);
+        //session.setAttribute("cname",coursename);
+        ModelAndView mv =new ModelAndView();
+        mv.addObject("cname", coursename);
+        mv.setViewName("course");
+        return mv;
+    }
 }
