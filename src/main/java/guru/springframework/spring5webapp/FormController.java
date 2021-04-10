33d24a05f5d2c8cfb52cd8d5590360afcd965296
dydko1 <miroslaw.dyduch@gmail.com>
2021-04-10 21:06:02 +0200

    add first api'

--- src/main/java/guru/springframework/spring5webapp/FormController.java
+++ src/main/java/guru/springframework/spring5webapp/FormController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -36,6 +37,12 @@ public class FormController {
 		Customers customers = repo.findById(cid).orElse(null);
 		mv.addObject(customers);
 		return mv;
+	}
+
+	@RequestMapping("/customers")
+	@ResponseBody
+	public String getCustomers() {
+		return repo.findAll().toString();
 	}
 
 	/*
