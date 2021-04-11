722cefe250bad55c450d904ba988eab7fd139c5d
dydko1 <miroslaw.dyduch@gmail.com>
2021-04-11 13:03:42 +0200

    Add rest + load sql data

--- src/main/java/guru/springframework/spring5webapp/CustomerRepo.java
+++ src/main/java/guru/springframework/spring5webapp/CustomerRepo.java
@@ -2,10 +2,11 @@ package guru.springframework.spring5webapp;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 //public interface CustomerRepo extends CrudRepository<Customers, Long> {
-@Repository
+@RepositoryRestResource(collectionResourceRel="customers", path="customers")
 public interface CustomerRepo extends JpaRepository<Customers, Long> {
 
 }
--- src/main/resources/sample.sql
+++ src/main/resources/sample.sql
@@ -1,2 +1,7 @@
-insert into Customers values (1,'miro@wp.pl','Miro');
+insert into customers values(1,'miro1@wp.pl','Miro1');
+insert into customers values(2,'miro2@wp.pl','Miro2');
+insert into customers values(3,'miro3@wp.pl','Miro3');
+insert into customers values(4,'miro4@wp.pl','Miro4');
+insert into customers values(5,'miro5@wp.pl','Miro5');
+insert into customers values(6,'miro5@wp.pl','Miro50');
 select * from customers;
