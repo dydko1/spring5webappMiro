642fdfb1f5e22a9eafa4ad6397c98b1002adc111
dydko1 <miroslaw.dyduch@gmail.com>
2021-04-11 08:10:56 +0200

    Finished rest part 1

--- src/main/java/guru/springframework/spring5webapp/CustomerRepo.java
+++ src/main/java/guru/springframework/spring5webapp/CustomerRepo.java
@@ -1,9 +1,11 @@
 package guru.springframework.spring5webapp;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepo extends CrudRepository<Customers, Long> {
+//public interface CustomerRepo extends CrudRepository<Customers, Long> {
+public interface CustomerRepo extends JpaRepository<Customers, Long> {
 
 }
--- src/main/java/guru/springframework/spring5webapp/FormController.java
+++ src/main/java/guru/springframework/spring5webapp/FormController.java
@@ -1,10 +1,17 @@
 package guru.springframework.spring5webapp;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -41,8 +48,38 @@ public class FormController {
 
 	@RequestMapping("/customers")
 	@ResponseBody
-	public String getCustomers() {
-		return repo.findAll().toString();
+	public List<Customers> getCustomers() {
+		return repo.findAll();
+		// return repo.findAll().toString();
+	}
+
+	@RequestMapping("/customers/{cid}")
+	@ResponseBody
+	public Optional<Customers> getCustomer(@PathVariable("cid") long cid) {
+		return repo.findById(cid);
+		// return repo.findById(cid).toString();
+	}
+
+	@PostMapping("/customers")
+	public Customers createCustomers(@RequestBody Customers customers) {
+		repo.save(customers);
+		return customers;
+		// return repo.findAll().toString();
+	}
+
+	@DeleteMapping("/customers/{cid}")
+	public Customers deleteCustomers(@PathVariable("cid") long cid) {
+		Customers cust = repo.getOne(cid);
+		repo.delete(cust);
+		return cust;
+		// return repo.findAll().toString();
+	}
+
+	@PutMapping(path = "/customers", consumes = { "application/json" })
+	public Customers putCustomers(@RequestBody Customers customers) {
+		repo.save(customers);
+		return customers;
+		// return repo.findAll().toString();
 	}
 
 	/*
