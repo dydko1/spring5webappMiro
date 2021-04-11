13b479ad28e33d3feb63004545ca893892938fa3
dydko1 <miroslaw.dyduch@gmail.com>
2021-04-11 11:58:41 +0200

    rest

--- src/main/java/guru/springframework/spring5webapp/CustomerRepo.java
+++ src/main/java/guru/springframework/spring5webapp/CustomerRepo.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
 //public interface CustomerRepo extends CrudRepository<Customers, Long> {
+@Repository
 public interface CustomerRepo extends JpaRepository<Customers, Long> {
 
 }
---
+++ src/main/resources/sample.sql
@@ -0,0 +1,2 @@
+insert into Customers values (1,'miro@wp.pl','Miro');
+select * from customers;
