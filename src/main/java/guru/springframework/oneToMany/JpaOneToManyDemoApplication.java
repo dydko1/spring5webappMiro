2ab14059912fe2eabf4fa3f4237a401a759f3b0b
dydko1 <miroslaw.dyduch@gmail.com>
2021-06-28 21:39:04 +0200

    oneToMay

--- src/main/java/guru/springframework/oneToMany/JpaOneToManyDemoApplication.java
+++ src/main/java/guru/springframework/oneToMany/JpaOneToManyDemoApplication.java
@@ -1,19 +1,16 @@
 package guru.springframework.oneToMany;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import java.util.Calendar;
-
 @SpringBootApplication
 @EnableJpaAuditing
-public class Spring5webappApplication implements CommandLineRunner {
+public class JpaOneToManyDemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5webappApplication.class, args);
+		SpringApplication.run(JpaOneToManyDemoApplication.class, args);
 	}
 
 	@Override
