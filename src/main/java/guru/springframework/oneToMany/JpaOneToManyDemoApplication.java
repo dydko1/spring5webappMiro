e3e98fd3a4c9497c51b7921b5e1496f260226bca
dydko1 <miroslaw.dyduch@gmail.com>
2021-06-30 22:25:43 +0200

    lesson finished

--- src/main/java/guru/springframework/oneToMany/JpaOneToManyDemoApplication.java
+++ src/main/java/guru/springframework/oneToMany/JpaOneToManyDemoApplication.java
@@ -1,32 +1,32 @@
 package guru.springframework.oneToMany;
 
-import guru.springframework.oneToMany.model.Comment;
-import guru.springframework.oneToMany.model.Post;
+import guru.springframework.oneToMany.model.Book;
+import guru.springframework.oneToMany.model.Page;
+import guru.springframework.oneToMany.repository.BookRepository;
+import guru.springframework.oneToMany.repository.PageRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
-public class JpaOneToManyDemoApplication implements CommandLineRunner {
+public class JpaOneToManyDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaOneToManyDemoApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
+	@Bean
+	public CommandLineRunner mappingDemo(BookRepository bookRepository, PageRepository pageRepository) {
+		return args -> {
+			Book book = new Book("Java 101", "John Doe", "123456");
 
-		/*
-		// Create a Post
-		Post post = new Post("post title", "post description", "post content");
+			bookRepository.save(book);
 
-		// Create Comments
-		Comment comment1 = new Comment("Great Post!");
-		comment1.setPost(post);
-		Comment comment2 = new Comment("Really helpful Post. Thanks a lot!");
-		comment2.setPost(post);
-	*/
+			pageRepository.save(new Page(1, "Introduction contents", "Introduction", book));
+			pageRepository.save(new Page(65, "Java 8 contents", "Java 8", book));
+			pageRepository.save(new Page(95, "Concurrency contents", "Concurrency", book));
+		};
 	}
 }
