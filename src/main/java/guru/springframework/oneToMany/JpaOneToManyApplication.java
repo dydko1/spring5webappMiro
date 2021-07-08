d61228ed7ccf9011fe4076af4ab47e2bbb2622bc
dydko1 <miroslaw.dyduch@gmail.com>
2021-07-08 13:21:38 +0200

    fin

--- src/main/java/guru/springframework/oneToMany/JpaOneToManyApplication.java
+++ src/main/java/guru/springframework/oneToMany/JpaOneToManyApplication.java
@@ -1,29 +1,35 @@
 package guru.springframework.oneToMany;
 
-import guru.springframework.oneToMany.model.Employee;
-import guru.springframework.oneToMany.repository.EmployeeRepository;
-import guru.springframework.oneToMany.repository.EmployeeRepositoryImpl;
+
+import guru.springframework.oneToMany.model.City;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 
 public class JpaOneToManyApplication {
+	private static final String PERSISTENCE_UNIT_NAME = "my-pu";
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaOneToManyApplication.class, args);
+
+
 	}
 
 	@Bean
-	public CommandLineRunner mappingDemo(EmployeeRepository employeeRepository) {
+	public CommandLineRunner mappingDemo() {
 		return args -> {
-			Employee employee = new Employee("Miroslaw", "Dyduch", "miroslaw.dyduch@gmial.com", 40, 45);
-			employeeRepository.save(employee);
+
 		};
 	}
 }
